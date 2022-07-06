1c0fb6f13d4c6adc69c8b35888964b1721ce0884
«Dasha» <orme94@mail.ru>
2022-07-06 15:17:42 +0300

    Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 26: Напишите программу, которая принимает
+//на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int DigitsCount(int num)
+{
+int count = 1;
+while (num > 9)
+{
+    num /= 10;
+    count++;
+}
+return count;
+}
+int res = DigitsCount(a);
+System.Console.WriteLine(res);
