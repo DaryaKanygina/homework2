c2042919527b452832a7f7b89a40c70a51a94c9f
«Dasha» <orme94@mail.ru>
2022-07-12 13:27:53 +0300

    Lesson_3

---
+++ lesson_3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] CreatArray(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
